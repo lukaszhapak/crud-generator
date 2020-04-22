fc30f06d12edd8885220622bb8db17ddba8f7c97
lhapak <lukaszrdw@gmail.com>
2020-04-22 14:57:27 +0200

    field class constructor added

--- src/main/java/sample/entity/Field.java
+++ src/main/java/sample/entity/Field.java
@@ -1,11 +1,12 @@
 package sample.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Field {
 
     private String type;
     private String name;
-
 }
