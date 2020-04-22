a30fafa5cab0e8eadc9a35854b2c602e2973fcf3
lhapak <lukaszrdw@gmail.com>
2020-04-22 14:36:33 +0200

    read file method in file operation helper

--- src/main/java/sample/file/FileOperationHelper.java
+++ src/main/java/sample/file/FileOperationHelper.java
@@ -3,8 +3,25 @@ package sample.file;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class FileOperationHelper {
+
+    public String getDataFromFile(String fileName) {
+        StringBuilder result = new StringBuilder();
+
+        try {
+            Scanner file = new Scanner(new File(fileName));
+            while (file.hasNextLine()) {
+                result.append(file.nextLine()).append("\n");
+            }
+            file.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return result.toString();
+    }
 
     public void saveDataInFile(String fileName, String data) {
         try {
